86a8b1617db772264915babd0928840c5e69f1d9
Yashsam63 <154651884+Yashsam63@users.noreply.github.com>
2024-09-05 22:23:10 +0530

    yash
    yash

--- app.py
+++ app.py
@@ -101,7 +101,3 @@ def gen_frames():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
-    #http://localhost:8000/video for video source
-    #http://localhost:8000 for image source
